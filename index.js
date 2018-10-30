6ebd940d45b8046e33cd2aef394035b512cfd4f9
dipayan <dipayan412@gmail.com>
2018-10-30 12:04:16 -0700

    v11

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ if (process.env.NODE_ENV === 'production') {
   // Express will serve up index.html file
   // if it doesn't recognize the route
   const path = require('path')
-  app.get('*', (res, req) => {
+  app.get('/', (res, req) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
   })
 }
